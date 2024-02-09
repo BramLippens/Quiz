a0750f11c3ec1ba148643568ed0860e4c1617fb8
Brampie <bram.lippens09@gmail.com>
2024-02-09 20:58:46 +0100

    extracted code from get req to function

--- src/controllers/qa.controller.js
+++ src/controllers/qa.controller.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+const GetOneQuestion = async (req, res) => {
   const question = {
     question: "Is this a question?",
     answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
   };
 
   res.json(question);
-});
+};
+
+router.get("/", GetOneQuestion);
 
 module.exports = router;
