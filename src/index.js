d03ff85e3f46b0b79c0bec6b872f110cdf1af2db
Brampie <bram.lippens09@gmail.com>
2024-02-09 20:47:26 +0100

    basic controller for http handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
+
+// Import the router from controllers
+const router = require("./controllers");
+
 const app = express();
 
+// Set the port to the environment variable PORT, or default to 3000
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Add the router to the /api endpoint
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}/`);
